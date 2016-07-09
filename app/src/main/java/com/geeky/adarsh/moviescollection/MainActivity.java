d18980311e089c6f92fc63868e3d8f8dd41d9c01
geekyAdarsh <adarshjain9628@gmail.com>
2016-07-10 00:06:30 +0530

    Test commit

--- app/src/main/java/com/geeky/adarsh/moviescollection/MainActivity.java
+++ app/src/main/java/com/geeky/adarsh/moviescollection/MainActivity.java
@@ -2,6 +2,7 @@ package com.geeky.adarsh.moviescollection;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appBar);
+        setSupportActionBar(toolbar);
     }
 }
