bc5d4d2dea28da475af2959c1fbd989c5b8aaa5f
geekyAdarsh <adarshjain9628@gmail.com>
2016-07-10 01:03:07 +0530

    Test commit

--- app/src/main/java/com/geeky/adarsh/moviescollection/MainActivity.java
+++ app/src/main/java/com/geeky/adarsh/moviescollection/MainActivity.java
@@ -1,17 +1,48 @@
 package com.geeky.adarsh.moviescollection;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.geeky.adarsh.moviescollection.ThirdParty.SlidingTabLayout;
+
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ViewPager mViewPager;
+    private SlidingTabLayout mSlidingTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.appBar);
+        toolbar = (Toolbar) findViewById(R.id.appBar);
         setSupportActionBar(toolbar);
+
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.tabs);
     }
+
+    class MyPagerAdapter extends FragmentPagerAdapter{
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
+
+    public static class MyFragment
 }
